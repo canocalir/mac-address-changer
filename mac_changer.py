e86745467c72cf1c88a1bfa7453edb64794b549b
Nefraim <can@ocalir.dev>
2020-02-13 12:33:46 -0500

    Added variables

--- mac_changer.py
+++ mac_changer.py
@@ -2,6 +2,14 @@
 
 import subprocess
 
-subprocess.call("ifconfig wlan0 down", shell=True)
-subprocess.call("ifconfig wlan0 hw ether 00:11:22:33:44:55:66", shell=True)
-subprocess.call("ifconfig wlan0 up", shell=True)
+#Replace value with device to change
+interface = "wlan0"
+
+#Replace value with desired MAC Address
+new_mac = "00:11:22:33:44:66"
+
+print("[+] Changing MAC Address for " + interface + " to " + new_mac)
+
+subprocess.call("ifconfig " + interface + " down", shell=True)
+subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
+subprocess.call("ifconfig " + interface + " up", shell=True)
