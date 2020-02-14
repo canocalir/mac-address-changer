c917eccb536059f480d39bc114a66367fd3bb081
Nefraim <can@ocalir.dev>
2020-02-14 13:40:01 -0500

    Error handling added

--- mac_changer.py
+++ mac_changer.py
@@ -7,7 +7,13 @@ def get_arguments ():
     parser = optparse.OptionParser()
     parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
     parser.add_option("-m", "--mac", dest="new_mac", help="New MAC Address")
-    return parser.parse_args()
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+        parser.error("[-] Please specify an interface, use --help for more info.")
+    elif not options.new_mac:
+        parser.error("[-] Please specify a new MAC, use --help for more info.")
+    return options
+
 
 def change_mac (interface, new_mac):
     print("[+] Changing MAC Address for " + interface + " to " + new_mac)
@@ -15,5 +21,6 @@ def change_mac (interface, new_mac):
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
 
-(options, arguments) = get_arguments()
+
+options = get_arguments()
 change_mac(options.interface, options.new_mac)
