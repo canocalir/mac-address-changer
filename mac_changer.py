2f86d59a194940a80bd084c729f92996db1899cd
Nefraim <can@ocalir.dev>
2020-02-14 12:31:24 -0500

    Wrote subprocess.call more secure way

--- mac_changer.py
+++ mac_changer.py
@@ -3,13 +3,13 @@
 import subprocess
 
 #Replace value with device to change
-interface = "wlan0"
+interface = input("interface > ")
 
 #Replace value with desired MAC Address
-new_mac = "00:11:22:33:44:66"
+new_mac = input("new MAC > ")
 
 print("[+] Changing MAC Address for " + interface + " to " + new_mac)
 
-subprocess.call("ifconfig " + interface + " down", shell=True)
-subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
-subprocess.call("ifconfig " + interface + " up", shell=True)
+subprocess.call(["ifconfig", interface, "down"])
+subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+subprocess.call(["ifconfig", interface, "up"])
