2006f7ff8fdf32915c5a9788d4fb94983e6ed08f
Nefraim <can@ocalir.dev>
2020-02-14 15:28:00 -0500

    Refactored and added security and validation features

--- mac_changer.py
+++ mac_changer.py
@@ -2,8 +2,10 @@
 
 import subprocess
 import optparse
+import re
 
-def get_arguments ():
+
+def get_arguments():
     parser = optparse.OptionParser()
     parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
     parser.add_option("-m", "--mac", dest="new_mac", help="New MAC Address")
@@ -15,12 +17,32 @@ def get_arguments ():
     return options
 
 
-def change_mac (interface, new_mac):
+def change_mac(interface, new_mac):
     print("[+] Changing MAC Address for " + interface + " to " + new_mac)
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
 
 
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+
+    if mac_address_search_result:
+        return mac_address_search_result.group(0)
+    else:
+        print("[-] Could not read MAC address.")
+
+
 options = get_arguments()
+
+current_mac = get_current_mac(options.interface)
+print("Current MAC = " + str(current_mac))
+
 change_mac(options.interface, options.new_mac)
+
+current_mac = get_current_mac(options.interface)
+if current_mac == options.new_mac:
+    print("[+] MAC address was successfully changed to " + current_mac)
+else:
+    print("[-] MAC address did not get changed")
